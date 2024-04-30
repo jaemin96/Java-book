a2d29c2eca7ac42c99d18bbdb67fb65d106217bf
jaemin96 <lemon__96@naver.com>
2024-04-30 16:00:03 +0900

    fix: util 함수 intInput 수정

--- JavaBook/src/main/java/algorithm/basic/math/util/Input.java
+++ JavaBook/src/main/java/algorithm/basic/math/util/Input.java
@@ -9,10 +9,10 @@ public class Input {
 
     public int intInput(Scanner sc,int min_range, int max_range) {
         int number;
+
         do {
-            System.out.print(">> ");
             number = sc.nextInt();
-            if(number<min_range || number>max_range) System.out.println("입력 범위를 확인하세요");
+            if(number<min_range || number>max_range) System.out.print("입력 범위를 확인하세요 >> ");
         }while(number<min_range || number>max_range);
 
         return number;
