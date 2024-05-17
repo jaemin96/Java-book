6c81a69341dfedd0f66240bcc0a1030e230af6f6
jaemin96 <lemon__96@naver.com>
2024-05-17 16:01:47 +0900

    오타 수정

--- JavaBook/src/main/java/algorithm/basic/math/data/LastValue.java
+++ JavaBook/src/main/java/algorithm/basic/math/data/LastValue.java
@@ -7,8 +7,8 @@ public class LastValue extends Que {
         int time = this.que.size(); // 반복할 횟수 지정
         if (!this.isQueEmpty()) {
             for (int i=0; i<time; i++) {
-                this.pop(); // 가장 아래 요소 삭제! 234 42
-                this.push(this.pop()); // 삭제 후 가장 아래 남은 요소를 가장 위로 옮기기 342 24
+                this.pop(); // 가장 아래 요소 삭제!
+                this.push(this.pop()); // 삭제 후 가장 아래 남은 요소를 가장 위로 옮기기
                 if(this.que.size() == 1) {
                     return this.pop();
                 }
