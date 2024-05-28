e7d2abe1bf3fd763ced0dee9ec04de3583a5b946
jaemin96 <lemon__96@naver.com>
2024-05-28 15:23:34 +0900

    feat: algorithm 예제 - backtrack + test

---
+++ JavaBook/src/main/java/algorithm/basic/math/backtracking/SequenceByNatural.java
@@ -0,0 +1,42 @@
+package algorithm.basic.math.backtracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SequenceByNatural {
+    public int sequenceCheckCases(int N, int M) {
+        List<Integer> sequence = new ArrayList<>(); // 수열 저장
+        List<List<Integer>> result = new ArrayList<>(); // 수열로 구성된 전체 결과 저장
+        boolean[] isUsed = new boolean[N+1]; // 검사하는 수 사용 여부 판단
+        backtrack(result, sequence, isUsed, N, M);
+
+        return result.size();
+    }
+
+    public List<List<Integer>> sequenceCheck(int N, int M) {
+        List<Integer> sequence = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
+        boolean[] isUsed = new boolean[N+1];
+        backtrack(result, sequence, isUsed, N, M);
+
+        return result;
+    }
+
+    private static void backtrack(List<List<Integer>> result, List<Integer> sequence, boolean[] isUsed, int N, int M) {
+        if(sequence.size() == M) {
+            result.add(new ArrayList<>(sequence));
+            return;
+        }
+
+        for (int i=1; i<=N; i++) {
+            if(!isUsed[i]){
+                isUsed[i] = true; // 해당 값이 사용됨
+                sequence.add(i); // 수열에 해당 값을 삽입
+                backtrack(result, sequence, isUsed, N, M);
+                sequence.remove(sequence.size() -1);
+                isUsed[i] = false;
+            }
+        }
+    }
+}
---
+++ JavaBook/src/test/java/algorithm/math/backtracking/SequenceTest.java
@@ -0,0 +1,56 @@
+package algorithm.math.backtracking;
+
+import algorithm.basic.math.backtracking.SequenceByNatural;
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SequenceTest {
+    @Nested
+    @DisplayName("Sequence by natural number")
+    class SequenceByNaturalNumber {
+        int N,M;
+        SequenceByNatural sbn;
+
+        @BeforeEach
+        void setup() {
+            N = 3;
+            M = 3;
+            sbn = new SequenceByNatural();
+        }
+
+        @Test
+        @DisplayName("it check number of cases")
+        void numberOfCases() {
+            int expected = 6;
+            int actual = this.sbn.sequenceCheckCases(this.N, this.M);
+            Assertions.assertEquals(expected,actual);
+        }
+
+        @Test
+        @DisplayName("it check if the lists match")
+        void matchLists() {
+            List<List<Integer>> results = new ArrayList<>();
+            results.add(Arrays.asList(1,2,3));
+            results.add(Arrays.asList(1,3,2));
+            results.add(Arrays.asList(2,1,3));
+            results.add(Arrays.asList(2,3,1));
+            results.add(Arrays.asList(3,1,2));
+            results.add(Arrays.asList(3,2,1));
+
+            List<List<Integer>> actual = this.sbn.sequenceCheck(this.N, this.M);
+
+            System.out.print("Expected list: ");
+            System.out.print(List.of(results));
+
+            System.out.println();
+
+            System.out.print("actual list: ");
+            System.out.print(List.of(actual));
+
+            Assertions.assertArrayEquals(results.toArray() ,actual.toArray());
+        }
+    }
+}
