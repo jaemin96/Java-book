ba9fc9a50a230021485e5a2266eb12623feda93c
jaemin96 <lemon__96@naver.com>
2024-05-29 10:48:15 +0900

    feat: algorithm 예제 - 백트래킹 중복허용 + test

--- JavaBook/src/main/java/algorithm/basic/math/backtracking/SequenceByNatural.java
+++ JavaBook/src/main/java/algorithm/basic/math/backtracking/SequenceByNatural.java
@@ -39,4 +39,39 @@ public class SequenceByNatural {
             }
         }
     }
+
+    public int sequenceCheckCasesIsDuplicate(int N, int M) {
+        List<Integer> sequence = new ArrayList<>(); // 수열 저장
+        List<List<Integer>> result = new ArrayList<>(); // 수열로 구성된 전체 결과 저장
+        boolean[] isUsed = new boolean[N+1]; // 검사하는 수 사용 여부 판단
+        backtrackDuplicate(result, sequence, isUsed, N, M);
+
+        return result.size();
+    }
+
+    public List<List<Integer>> sequenceCheckIsDuplicate(int N, int M) {
+        List<Integer> sequence = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
+        boolean[] isUsed = new boolean[N+1];
+        backtrackDuplicate(result, sequence, isUsed, N, M);
+
+        return result;
+    }
+
+    public static void backtrackDuplicate(List<List<Integer>> result, List<Integer> sequence, boolean[] isUsed, int N, int M) {
+        if(sequence.size() == M) {
+            result.add(new ArrayList<>(sequence));
+            return;
+        }
+
+        for (int i=1; i<=N; i++) {
+            if(!isUsed[i]){
+                sequence.add(i); // 수열에 해당 값을 삽입
+                backtrackDuplicate(result, sequence, isUsed, N, M);
+                isUsed[i] = true; // 해당 값이 사용됨
+                sequence.remove(sequence.size() -1);
+                isUsed[i] = false;
+            }
+        }
+    }
 }
--- JavaBook/src/test/java/algorithm/math/backtracking/SequenceTest.java
+++ JavaBook/src/test/java/algorithm/math/backtracking/SequenceTest.java
@@ -52,5 +52,58 @@ public class SequenceTest {
 
             Assertions.assertArrayEquals(results.toArray() ,actual.toArray());
         }
+
+        @Test
+        @DisplayName("it checks number of cases allowing duplicate numbers")
+        void numberOfCasesAllowDuplicate() {
+            int expected = 27;
+            int actual = this.sbn.sequenceCheckCasesIsDuplicate(this.N, this.M);
+            Assertions.assertEquals(expected, actual);
+        }
+
+        @Test
+        @DisplayName("it check if the lists match allowing duplicate numbers")
+        void matchListsAllowDuplicate() {
+            List<List<Integer>> results = new ArrayList<>();
+            results.add(Arrays.asList(1,1,1));
+            results.add(Arrays.asList(1,1,2));
+            results.add(Arrays.asList(1,1,3));
+            results.add(Arrays.asList(1,2,1));
+            results.add(Arrays.asList(1,2,2));
+            results.add(Arrays.asList(1,2,3));
+            results.add(Arrays.asList(1,3,1));
+            results.add(Arrays.asList(1,3,2));
+            results.add(Arrays.asList(1,3,3));
+            results.add(Arrays.asList(2,1,1));
+            results.add(Arrays.asList(2,1,2));
+            results.add(Arrays.asList(2,1,3));
+            results.add(Arrays.asList(2,2,1));
+            results.add(Arrays.asList(2,2,2));
+            results.add(Arrays.asList(2,2,3));
+            results.add(Arrays.asList(2,3,1));
+            results.add(Arrays.asList(2,3,2));
+            results.add(Arrays.asList(2,3,3));
+            results.add(Arrays.asList(3,1,1));
+            results.add(Arrays.asList(3,1,2));
+            results.add(Arrays.asList(3,1,3));
+            results.add(Arrays.asList(3,2,1));
+            results.add(Arrays.asList(3,2,2));
+            results.add(Arrays.asList(3,2,3));
+            results.add(Arrays.asList(3,3,1));
+            results.add(Arrays.asList(3,3,2));
+            results.add(Arrays.asList(3,3,3));
+
+            List<List<Integer>> actual = this.sbn.sequenceCheckIsDuplicate(this.N, this.M);
+
+            System.out.print("Expected list: ");
+            System.out.print(List.of(results));
+
+            System.out.println();
+
+            System.out.print("actual list: ");
+            System.out.print(List.of(actual));
+
+            Assertions.assertArrayEquals(results.toArray(), actual.toArray());
+        }
     }
 }
